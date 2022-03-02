067a34c92b25282c49e22e076cc734572a7de652
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2022-03-03 01:20:06 +0700

    Code giai tri

---
+++ 26.c/1.cpp
@@ -0,0 +1,50 @@
+
+/*
+    Đề bài: Cho xâu s được nhập từ bàn phím, bạn hãy viết hàm chuyển các ký tự in thường 
+            trong xâu s thành in hoa và hiển thị xâu s ra màn hình.
+
+*/
+
+// sử dụng tham trị
+#include <iostream>
+
+using namespace std;
+
+void toUpper(string &s) {
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 'a' && s[i] <= 'z') {
+			s[i] -= 32;
+		}
+	}
+}
+
+int main() {
+	string s;
+	cin >> s;
+	toUpper(s);
+	cout << s;
+	return 0;
+}
+
+
+
+// sự dụng tham chiếu
+#include <iostream>
+
+using namespace std;
+
+void toUpper(string *s) {
+	for (int i = 0; i < (*s).size(); i++) {
+		if ((*s)[i] >= 'a' && (*s)[i] <= 'z') {
+			(*s)[i] -= 32;
+		}
+	}
+}
+
+int main() {
+	string s;
+	cin >> s;
+	toUpper(&s);
+	cout << s;
+	return 0;
+}
