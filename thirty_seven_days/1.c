9e3816becf51ecbe46fb945ea998321c70cb2ddf
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-03-18 01:11:04 +0700

    Tien 18/03/2022

---
+++ thirty_seven_days/1.c
@@ -0,0 +1,26 @@
+
+
+
+/*
+
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+arr_integer solution(int lastNumberdays) {
+    arr_integer days;
+    if (lastNumberdays == 28 || lastNumberdays == 30) {
+        arr_integer days = alloc_arr_integer(1);
+        days.arr[0] = 31;
+        return days;
+    } else {
+        arr_integer days = alloc_arr_integer(3);
+        days.arr[0] = 28;
+        days.arr[1] = 30;
+        days.arr[2] = 31;
+        return days;
+    }
+    return days;
+}
+
